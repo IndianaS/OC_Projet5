16772a155c94bd6af846dfec08372496da88a32a
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-01-08 14:18:20 +0100

    Add method main, add docstring

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from controller.interface import Interface
 from models.bdd.connection import Connection
 
+def __main__():
+    auth = Connection()
+    auth.connect()
 
-auth = Connection()
-auth.connect()
+    application = Interface(auth)
+    application.loop()
 
-application = Interface(auth)
-application.loop()
+if __name__ == "__main__":
+    __main__()
--- installdb.py
+++ installdb.py
@@ -3,25 +3,30 @@ from models.bdd.dbadd import DbAdd
 from models.bdd.dbcreate import DbCreate
 from models.categorydownloader import CategoryDownloader
 
-auth = Connection()
-auth.connect()
 
-# Creation of the comic, creation of the tables
-create_db = DbCreate(auth)
-print('On crée la base de données !')
-create_db.create_database()
-print('On crée les tables !')
-create_db.create_table()
+def __main__():
+    auth = Connection()
+    auth.connect()
 
-# Download the categories then the ranges
-category = CategoryDownloader()
-print('On télécharge les catégories et les produits !')
-all_categories = category.get_category()
+    # Creation of the comic, creation of the tables
+    create_db = DbCreate(auth)
+    print('On crée la base de données !')
+    create_db.create_database()
+    print('On crée les tables !')
+    create_db.create_table()
 
-add_db = DbAdd(auth)
-# Add the categories to the database
+    # Download the categories then the ranges
+    category = CategoryDownloader()
+    print('On télécharge les catégories et les produits !')
+    all_categories = category.get_category()
 
-print('On ajoute les catégories en base de données !')
-add_db.add_category(all_categories)
-print('On ajoute les produits de chaque catégorie en base de données !')
-add_db.add_product(all_categories)
+    add_db = DbAdd(auth)
+    # Add the categories to the database
+
+    print('On ajoute les catégories en base de données !')
+    add_db.add_category(all_categories)
+    print('On ajoute les produits de chaque catégorie en base de données !')
+    add_db.add_product(all_categories)
+
+if __name__ == "__main__":
+    __main__()
